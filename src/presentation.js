ac82074bbaa34de2b8c5f9273b30c7d869b97786
jamie <jamie.parkinson@techwillsaveus.com>
2018-04-15 12:15:56 +0100

    Introductory section done

--- src/presentation.js
+++ src/presentation.js
@@ -3,33 +3,53 @@ import React from 'react';
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
+  Layout,
+  Fill,
   Deck,
   Heading,
   ListItem,
   List,
-  Quote,
   Slide,
-  Text,
+  Image,
+  Appear,
+  Text
 } from 'spectacle';
 
 // Import theme
 import createTheme from 'spectacle/lib/themes/default';
 
+import me from './assets/headshot.jpg';
+import blob1 from './assets/blob-1.svg';
+import blob2 from './assets/blob-2.svg';
+import blob3 from './assets/blob-3.svg';
+import styled from "styled-components";
+
 // Require CSS
 require('normalize.css');
+
+const CircleImage = styled(Image)`
+  border: 6px solid rgb(80, 50, 180);
+  border-radius: 50%;
+`;
+
+const AppearingListItem = ({children, ...props}) => (
+  <Appear>
+    <ListItem padding="10px 0" {...props}>
+      {children}
+    </ListItem>
+  </Appear>
+);
 
 const theme = createTheme(
   {
     primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
+    secondary: 'rgb(80, 50, 180)',
+    tertiary: '#ffaac8',
     quartenary: '#CECECE',
   },
   {
-    primary: 'Montserrat',
-    secondary: 'Helvetica',
+    primary: 'Maax',
+    secondary: 'Super Grotesk OT',
   }
 );
 
@@ -41,53 +61,94 @@ export default class Presentation extends React.Component {
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+        <Slide transition={['zoom']} bgColor="secondary">
+          <Heading size={1} caps textFont="secondary" lineHeight={1} textColor="primary">
+            Animations in React Native
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="40px 0 0" textColor="primary" size={1} fit>
+            or: How I Learned to Stop Worrying and Love Native UX
           </Text>
         </Slide>
         <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
+          <Heading size={6} textColor="secondary">Me</Heading>
+          <CircleImage margin="30px auto" src={me} height={300} />
+          <Text margin="40px 0 0" textColor="secondary">@jamieparkinson</Text>
+          <Text margin="20px 0 0" textColor="secondary">Technology Will Save Us</Text>
         </Slide>
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
+          <Heading size={4} textColor="secondary">
+            What makes an app feel native?
+            <List>
+              <AppearingListItem>Works offline?</AppearingListItem>
+              <AppearingListItem>Uses device hardware?</AppearingListItem>
+              <AppearingListItem>Doesn't need to fetch its layout?</AppearingListItem>
+              <AppearingListItem>Gestures?</AppearingListItem>
+              <AppearingListItem>Animations?</AppearingListItem>
+            </List>
           </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
         </Slide>
         <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+          <Heading size={5} textColor="primary">Is this where React Native falls down?</Heading>
+          <Appear>
+            <Heading margin="50px 0 0" size={3} textColor="tertiary">(No)</Heading>
+          </Appear>
+        </Slide>
+        <Slide bgColor="tertiary" textColor="secondary">
+          <Heading size={3} textColor="secondary">
+            A tale of two development cultures
+          </Heading>
+          <Layout>
+            <Fill>
+              <Heading
+                size={4}
+                bgColor="primary"
+                textColor="secondary"
+                padding="50px 25px 37px 25px"
+                margin={40}
+                lineHeight={2}
+              >
+                Web
+              </Heading>
+            </Fill>
+            <Fill>
+              <Heading
+                size={4}
+                bgColor="primary"
+                textColor="secondary"
+                padding="50px 25px 37px 25px"
+                margin={40}
+                lineHeight={2}
+              >
+                Native
+              </Heading>
+            </Fill>
+          </Layout>
+        </Slide>
+        <Slide bgColor="primary" transition={['fade']}>
+          <Heading size={5} textColor="secondary">
+            Animation is not a luxury.
+          </Heading>
+        </Slide>
+        <Slide bgColor="primary" transition={['fade']}>
+          <Appear
+            startValue={{ opacity: 1, transform: 'translate(-84px)' }}
+            endValue={{ opacity: 1, transform: 'translate(0px)' }}
+          >
+            <Heading size={5} textColor="secondary">
+              Animation is easy!
+            </Heading>
+          </Appear>
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary">
+          <Image margin="20px auto" src={blob1} />
+          <Layout>
+            <Fill>
+              <Image margin={20} src={blob2} />
+            </Fill>
+            <Fill>
+              <Image margin={20} src={blob3} />
+            </Fill>
+          </Layout>
         </Slide>
       </Deck>
     );
